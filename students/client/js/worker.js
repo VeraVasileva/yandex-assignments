3489d7a392f2406c106a97fa81e582f93555a516
Vera Vasileva <vs.vasileva@hotmail.com>
2016-05-18 12:56:33 +0300

    Update worker.js

--- students/client/js/worker.js
+++ students/client/js/worker.js
@@ -3,7 +3,8 @@ var CACHE_NAME = 'shri-2016-task3-1';
 var urlsToCache = [
   './',
   './css/index.css',
-  './js/index.js'
+  './js/index.js',
+  '../server/students.js'
 ];
 
 self.addEventListener('install', (event) => {
